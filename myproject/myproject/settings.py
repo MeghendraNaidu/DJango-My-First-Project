b675a661dff37f58fb06bb18064e0e5ae4e6f2b8
MeghendraNaidu <meghendranarravula@Gmail.com>
2026-02-07 12:12:01 +0530

    New Things Updated

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-zzfg4t18@e#bzs4b$rt-7nfor@&eg_f*ri52x7oil9ylpvw4rq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangomyfirstproject.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['djangomyfirstproject.onrender.com', '127.0.0.1', 'django-my-first-project.onrender.com']
 
 
 # Application definition
